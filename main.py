dc6abd7440d90e7a863f119dc1d3465dd2fc8bef
sspandit14 <sspandit@usc.edu>
2024-04-24 03:54:26 -0700

    updated main

--- main.py
+++ main.py
@@ -3,27 +3,49 @@
 import time
 import hashlib
 
+def crack_password(hash, current_guess, password_found, depth):
+    if password_found[2] == True:
+        print(password_found[1])
+        return
+    if password_found[0] == depth:
+        return
+    for i in range (32, 128):
+        new_guess = current_guess + chr(i)
+        hashed_guess = (hashlib.md5(new_guess.encode())).hexdigest()
+        if hash == hashed_guess:
+            password_found[1] = new_guess
+            password_found[2] = True
+            print(new_guess)
+            return
+        else:
+            crack_password(hash, new_guess, password_found, depth+1)
+        
+
 pass_the_hash = open("hashes.txt", "r")
 hash_to_crack = pass_the_hash.readlines()
+print(hash_to_crack)
 times_to_crack = []
 
 for hash in hash_to_crack:
     start = time.time()
 
-    while guess_hash != hash:
-        #make a recursive brute force lmao
+    hash = hash[:-1]
+    print(hash)
+
+    current_guess = ""
+    password_found = [1, None, False]
+
+    print("cracking password...")
+    while password_found[2] == False:
+        crack_password(hash, current_guess, password_found, 0)
+        ++password_found[0]
 
     end = time.time()
 
     time_taken = end-start
 
-    print(time_taken + " seconds taken")
+    print(f'{time_taken} seconds taken')
 
-def crack_password(hash, current_guess, new_char):
-    new_guess = current_guess + new_char
-    new_guess = (hashlib.md5(new_guess.encode())).hexdigest()
+    times_to_crack.append(time_taken)
 
-    if hash == new_guess:
-        return True
-    else
-        return False
+print("all passwords cracked!")
