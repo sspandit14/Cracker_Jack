1415ff8fcecdd534317e8baf9880b93c57452efe
sspandit14 <sspandit@usc.edu>
2024-04-24 22:27:33 -0700

    modified naive cracker working

--- main.py
+++ main.py
@@ -3,49 +3,86 @@
 import time
 import hashlib
 
-def crack_password(hash, current_guess, password_found, depth):
-    if password_found[2] == True:
-        print(password_found[1])
+def crack_password(hash, password_found, guesses_so_far):
+    if password_found[1] == True:
         return
-    if password_found[0] == depth:
+
+    next_set_of_guesses = []
+    #chars_to_guess = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789~!@#$%&*"
+    chars_to_guess = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%&*"
+    for guess in guesses_so_far:
+        for i in chars_to_guess:
+            new_guess = guess + i
+            #print(new_guess)
+            hashed_guess = (hashlib.md5(new_guess.encode())).hexdigest()
+            if hash == hashed_guess:
+                password_found[0] = new_guess
+                password_found[1] = True
+                next_set_of_guesses.append(new_guess)
+                return
+            else:
+                next_set_of_guesses.append(new_guess)
+
+    crack_password(hash, password_found, next_set_of_guesses)
+
+def crack_password_two(hash_to_crack, guesses_so_far, num_cracked, pass_found, start):
+    if num_cracked == len(hash_to_crack):
         return
-    for i in range (32, 128):
-        new_guess = current_guess + chr(i)
-        hashed_guess = (hashlib.md5(new_guess.encode())).hexdigest()
-        if hash == hashed_guess:
-            password_found[1] = new_guess
-            password_found[2] = True
-            print(new_guess)
-            return
-        else:
-            crack_password(hash, new_guess, password_found, depth+1)
-        
+
+    next_set_of_guesses = []
+    chars_to_guess = "0123456789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ~!@#$%&*"
+    for guess in guesses_so_far:
+        for i in chars_to_guess:
+            new_guess = guess + i
+            #print(new_guess)
+            hashed_guess = (hashlib.md5(new_guess.encode())).hexdigest()
+            if hashed_guess in hash_to_crack:
+                num_cracked += 1
+                pass_found.append([new_guess, time.time()-start])
+                print(pass_found)
+            
+            next_set_of_guesses.append(new_guess)
+
+    crack_password_two(hash_to_crack, next_set_of_guesses, num_cracked, pass_found, start)
+
 
 pass_the_hash = open("hashes.txt", "r")
 hash_to_crack = pass_the_hash.readlines()
-print(hash_to_crack)
-times_to_crack = []
+pass_and_times = []
+
+for i in range(0, len(hash_to_crack)):
+    hash_to_crack[i] = hash_to_crack[i][:-1]
+
+guesses_so_far = [""]
+
+i = 1
 
 for hash in hash_to_crack:
     start = time.time()
 
-    hash = hash[:-1]
-    print(hash)
+    password_found = [None, False]
 
-    current_guess = ""
-    password_found = [1, None, False]
+    print(f'cracking password #{i}...')
 
-    print("cracking password...")
-    while password_found[2] == False:
-        crack_password(hash, current_guess, password_found, 0)
-        ++password_found[0]
+    guesses_so_far = [""]
+
+    crack_password(hash, password_found, guesses_so_far)
+
+    print(f'Password is: {password_found[0]}')
 
     end = time.time()
 
     time_taken = end-start
 
-    print(f'{time_taken} seconds taken')
+    print(f'{time_taken} seconds taken\n')
 
-    times_to_crack.append(time_taken)
+    pass_times = [password_found[0], time_taken]
+    pass_and_times.append(pass_times)
+
+    i += 1
 
 print("all passwords cracked!")
+print("times taken per password:")
+
+for t in pass_and_times:
+    print(f'Password: \'{t[0]}\' took {t[1]} seconds to crack')
